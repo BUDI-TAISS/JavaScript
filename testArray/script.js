5d469ba72fa646e0f81edaaa4e816ff77eea92fd
BUDI-TAISS <budi955858@gmail.com>
2021-04-17 22:55:59 +0700

    membuat array multidimensi atau array bersarang

--- testArray/script.js
+++ testArray/script.js
@@ -1,3 +1,4 @@
-var hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
-var binatang = ['Kucing', 'Kelinci', 'Monyet', 'Panda', 'Koala', 'Sapi']
-console.log(binatang[1])
+var hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+var binatang = ['Kucing', 'Kelinci', 'Monyet', 'Panda', 'Koala', 'Sapi'];
+var myArr= ['teks', 2, false, [1, 2, 3]];
+console.log(myArr[3][2])
