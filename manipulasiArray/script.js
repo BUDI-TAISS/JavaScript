c9a64f57caf48e01a8d2fd1bf293ebc9db527509
BUDI-TAISS <budi955858@gmail.com>
2021-04-18 09:37:17 +0700

    Menghapus isi array dengan cara manual

--- manipulasiArray/script.js
+++ manipulasiArray/script.js
@@ -2,8 +2,15 @@
 
 // 1. Menambah isi array
 
-var arr = []
-arr[0] = 'Sandhika'
-arr[1] = 'Budi'
-arr[2] = 'Ika'
+// var arr = []
+// arr[0] = 'Sandhika'
+// arr[1] = 'Budi'
+// arr[2] = 'Ika'
+// arr[3] = 'Anggun'
+
+// console.log(arr)
+
+//Menghapus isi array
+var arr = ['Sandhika', 'Budi', 'Hanan']
+arr[1] = undefined
 console.log(arr)
