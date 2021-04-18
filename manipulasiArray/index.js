0f885c1f6c9be753cdbcb007f064ea7158e83ff1
BUDI-TAISS <budi955858@gmail.com>
2021-04-18 09:19:58 +0700

    Rename index.js to scrip.js and create index.html

--- manipulasiArray/index.js
+++
@@ -1,6 +0,0 @@
-//Manipulasi Array
-
-//Menambah isi array
-
-var arr = ['a', 2, true]
-console.log(arr[1])
---
+++ manipulasiArray/script.js
@@ -0,0 +1,6 @@
+// Manipulasi Array
+
+// 1. Menambah isi array
+
+var arr = []
+console.log(arr[1])
